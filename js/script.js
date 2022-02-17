dddfbb320ac25f2d2f90aecc4ebe6283c62befc2
davidG22-prog <davidunuekokigho@gmail.com>
2022-02-17 12:41:45 +0100

    an update to elabs

--- js/script.js
+++ js/script.js
@@ -3,25 +3,24 @@ var dropdown = document.getElementsByClassName("dropdown-btn");
 let arrowUp = document.getElementById("arup");
 let arrowDn = document.getElementById("ardn");
 var i;
-let navName = document.getElementById('navName');
-var sideIcon = document.getElementById("sydIcon");
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
+  let clickedNode = dropdown[i];
+  clickedNode.addEventListener("click", function() {
   this.classList.toggle("active");
   var dropdownContent = this.nextElementSibling;
   if (dropdownContent.style.display === "block") {
   dropdownContent.style.display = "none";
-  arrowDn.style.display = "block";
-  arrowUp.style.display = "none";
-  navName.classList.toggle('navName_active');
-  sideIcon.classList.toggle('side-icon');
+    clickedNode.children[0].classList.remove('side-icon');
+    clickedNode.children[1].classList.remove('navName_active');
+    clickedNode.children[2].children[0].style.display = "block";
+    clickedNode.children[2].children[1].style.display = "none";
   } else {
   dropdownContent.style.display = "block";
-  arrowDn.style.display = "none";
-  arrowUp.style.display = "block";
-  navName.classList.toggle('navName_active');
-  sideIcon.classList.toggle('side-icon');
+  clickedNode.children[0].classList.add('side-icon');
+  clickedNode.children[1].classList.add('navName_active');
+  clickedNode.children[2].children[0].style.display = "none";
+  clickedNode.children[2].children[1].style.display = "block";
   }
   });
 }
@@ -34,10 +33,47 @@ const drop = () => {
 		dropMenu.style.visibility = "hidden";
 	}
 }
+
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
 
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
+}
+
+function toggleNav() { 
+  let wSyd = document.getElementById("webSyd");
+  let wMain = document.getElementById("webMain");
+  let wBtm = document.getElementById("webBtm");
+
+  if((wSyd.style.width != "2.65rem") && (document.body.style.minWidth > "600px") ){
+    wSyd.style.width = "2.65rem";
+    wMain.style.marginLeft = "3rem";
+    wBtm.style.marginLeft = "3rem";
+    let sideNames = document.getElementsByClassName("navName");
+      for (let j = 0; j < sideNames.length; j++){
+        sideNames[j].style.display = "none";
+      }
+    let sideArrows = document.getElementsByClassName("arow");
+      for (let k = 0; k < sideNames.length; k++){
+        sideArrows[k].style.display = "none";
+      } 
+    }else{
+    wSyd.style.width = "20%";
+    wMain.style.marginLeft = "20.75%";
+    wBtm.style.marginLeft = "20.75%";
+    let sideNames = document.getElementsByClassName("navName");
+      for (let j = 0; j < sideNames.length; j++){
+        setTimeout(()=>{
+        sideNames[j].style.display = "inline-block";
+        },500);
+      }
+    let sideArrows = document.getElementsByClassName("arow");
+      for (let k = 0; k < sideNames.length; k++){
+        setTimeout(()=>{
+        sideArrows[k].style.display = "inline-block";
+      },500);
+      }
+    }   
 }
